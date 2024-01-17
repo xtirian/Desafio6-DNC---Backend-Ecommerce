7c91e6c0329d700e6227d90b2deb4267a12a0ded
xTirian <tirianx@yahoo.com>
2024-01-17 18:37:55 -0300

    add product route

---
+++ src/controllers/product.controller.ts
@@ -0,0 +1,44 @@
+import { Decimal } from "@prisma/client/runtime/library";
+import { prisma } from "../utils/prisma";
+
+interface ProductInterface {
+  id: number;
+  name: string;
+  price: Decimal;
+  cost: Decimal;
+  quantity: number;
+}
+
+export class ProductController {
+  static async CheckProductExist(
+    name: string
+  ): Promise<ProductInterface[] | undefined> {
+    const productExist = await prisma.product.findMany({
+      where: {
+        name: name,
+      },
+    });
+
+    if (productExist) {
+      return productExist;
+    } else {
+      return;
+    }
+  }
+
+  static async GetProductById(
+    id: number
+  ): Promise<ProductInterface | undefined> {
+    const savedProduct = await prisma.product.findUnique({
+      where: {
+        id: id,
+      },
+    });
+
+    if (savedProduct) {
+      return savedProduct;
+    }
+
+    return;
+  }
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,15 +1,16 @@
 import { Express, Router } from "express";
 import CustomerRoute from './routes/customer.route'
+import ProductRouter from './routes/product.route'
 
 function routes(server: Express): void {
-  try {
-    
+  try {    
 
     server.use("/customer", CustomerRoute);
+    server.use("/product", ProductRouter);
 
     return;
   } catch (error) {
-    console.error(error);
+    throw error;
   }
 }
 
